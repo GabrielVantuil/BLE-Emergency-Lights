5882f22ed75b6593a9806082169941e4f32f8d43
GabrielVantuil <gabrieltvantuil@gmail.com>
2025-05-21 19:09:12 -0300

    First (simple) working code

---
+++ Code/src/board_config.h
@@ -0,0 +1,35 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define LED_ON	1
+#define LED_OFF	0
+
+//#define LED_CTRL_PIN    18
+//#define IR_SENSOR_PIN   5 //AIN3
+//#define IR_LED_PIN      0
+
+#define WING1_PIN	   	00
+#define WING2_PIN	   	01
+
+#define MEAS_BAT_PIN    04
+#define SHUNT_PIN	   	05
+#define BUTTON_PIN	   	06
+
+#define SCL_PIN	       	09
+#define SDA_PIN		   	10
+
+#define SENSOR_PWR_PIN	12
+
+#define LED_R_PIN      	14
+#define LED_G_PIN      	15
+#define LED_B_PIN      	16
+
+#define MAIN_LED_CTRL_PIN    18
+#define WING1_LED_CTRL_PIN   20
+#define WING2_LED_CTRL_PIN   21
+
+#define INPUT_V_PIN     30
+#define LDR_PIN         28
+#define RGB_COMMON_PIN  25
+
+#endif
